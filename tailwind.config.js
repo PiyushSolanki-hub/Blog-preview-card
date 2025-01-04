2a187b01e46b4033dd64d767ff22b933898ae52e
Piyush Solanki <piyushsolanki2521@gmail.com>
2025-01-04 16:15:13 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Figtree':['Figtree','Serif'],
+      },
+      boxShadow:{
+        '8px':'8px 8px'
+      }
+    },
+  },
+  plugins: [],
+}
+
